33d398bde3fd8b401bb614d6dc4239e9ed0eb0f4
annabeldied <tanyalitvinenko976@gmail.com>
2016-05-15 21:55:04 +0300

    second try

--- lib/Commands/application.php
+++ lib/Commands/application.php
@@ -1,1 +1,11 @@
+#!/usr/bin/env php
 <?php
+// application.php
+
+require __DIR__.'/../../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+$application->add(new Acme\Console\Command\ResizeCommand());
+$application->run();
